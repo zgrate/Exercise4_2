4e12e9135622c627040e9674eba8e5dae51df891
Z-Grate <supergamers.pl@gmail.com>
2022-05-16 10:37:57 +0200

    asdasd
    asd

---
+++ AsyncComplexCalc/AsyncComplexCalculator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace AsyncCallbackService
+{
+
+    public interface IComplexCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void CalculationResult(string operation, AsyncComplexNumber result);
+    }
+
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract(SessionMode = SessionMode.Required, CallbackContract = typeof(IComplexCallback))]
+    public interface AsyncComplexCalculator
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        AsyncComplexNumber AddComplex(AsyncComplexNumber number1, AsyncComplexNumber number2);
+        
+        [OperationContract]
+        AsyncComplexNumber MultipleComplex(AsyncComplexNumber number1, AsyncComplexNumber number2);
+
+        // TODO: Add your service operations here
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "AsyncComplexCalc.ContractType".
+    [DataContract]
+    public class AsyncComplexNumber
+    {
+        double real = 0;
+        double imaginary = 0;
+
+        [DataMember]
+        public double RealValue
+        {
+            get { return real; }
+            set { real = value; }
+        }
+
+        [DataMember]
+        public double ImaginaryValue
+        {
+            get { return imaginary; }
+            set { imaginary = value; }
+        }
+    }
+}
---
+++ AsyncComplexCalc/Service1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace AsyncCallbackService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : AsyncComplexCalculator
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public AsyncComplexNumber GetDataUsingDataContract(AsyncComplexNumber composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
